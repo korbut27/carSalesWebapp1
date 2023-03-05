efe2122afe8bfaaff06058aa0a054e131b3dc2a3
korbut27 <korbut2702@gmail.com>
2023-03-05 14:01:34 +0300

    Initial commit

---
+++ src/main/java/com/example/carSalesWebapp1/controller/RegistrationController.java
@@ -0,0 +1,40 @@
+package com.example.carSalesWebapp1.controller;
+
+
+import com.example.carSalesWebapp1.domain.Role;
+import com.example.carSalesWebapp1.repos.UserRepo;
+import com.example.carSalesWebapp1.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}
